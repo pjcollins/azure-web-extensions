847ef89e407992cd98520bfba691687e49792d87
Peter Collins <peter.collins@xamarin.com>
2019-07-23 18:50:22 -0400

    Don't install Boots as a global tool

--- Tasks/Boots/boots.ts
+++ Tasks/Boots/boots.ts
@@ -1,14 +1,22 @@
 import tl = require('azure-pipelines-task-lib/task');
+import path = require('path');
 
 async function run() {
     try {
         const uriToInstall: string = tl.getInput('uri', true);
 
-        var dotnet = tl.tool('dotnet');
-        dotnet.line('tool install --global boots --version 0.1.0.251-beta');
-        await dotnet.exec();
+        var tmpDir = tl.getVariable('Agent.TempDirectory');
+        var bootsToolDir = path.join(tmpDir, 'boots-tool');
+        var bootsTool = tl.which(path.join(bootsToolDir, 'boots'));
 
-        var boots = tl.tool('boots');
+        if (!bootsTool) {
+            var dotnet = tl.tool('dotnet');
+            dotnet.line('tool install boots --version 0.1.0.251-beta --tool-path ' + bootsToolDir);
+            await dotnet.exec();
+            bootsTool = tl.which(path.join(bootsToolDir, 'boots'));
+        }
+
+        var boots = tl.tool(bootsTool);
         boots.line(uriToInstall);
         await boots.exec();
     }
