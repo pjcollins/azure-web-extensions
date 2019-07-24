646862b75d6987961b162fc4743db57e62cceb93
Peter Collins <pecolli@microsoft.com>
2019-07-23 23:22:32 -0400

    Set DOTNET_ROOT to fix Boots usage on Mac (#3)

--- Tasks/Boots/boots.ts
+++ Tasks/Boots/boots.ts
@@ -1,5 +1,6 @@
 import tl = require('azure-pipelines-task-lib/task');
 import path = require('path');
+import { readlinkSync } from 'fs';
 
 async function run() {
     try {
@@ -8,12 +9,25 @@ async function run() {
         var tmpDir = tl.getVariable('Agent.TempDirectory');
         var bootsToolDir = path.join(tmpDir, 'boots-tool');
         var bootsTool = tl.which(path.join(bootsToolDir, 'boots'));
+        var dotnetTool = tl.which('dotnet');
 
         if (!bootsTool) {
-            var dotnet = tl.tool('dotnet');
+            var dotnet = tl.tool(dotnetTool);
             dotnet.line('tool install boots --version 0.1.0.251-beta --tool-path ' + bootsToolDir);
             await dotnet.exec();
             bootsTool = tl.which(path.join(bootsToolDir, 'boots'));
+        }
+
+        // Workaround error 'the required library libhostfxr.dylib could not be found.'
+        // https://github.com/dotnet/cli/issues/9114#issuecomment-494226139
+        if (tl.getPlatform() === tl.Platform.MacOS) {
+            try {
+                var dotnetToolLink = readlinkSync(dotnetTool);
+                if (typeof dotnetToolLink == 'string') {
+                    tl.setVariable("DOTNET_ROOT", path.dirname(dotnetToolLink));
+                }
+            } catch (err) {
+            }
         }
 
         var boots = tl.tool(bootsTool);
